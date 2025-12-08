b43db1a204ff501ba9ccc2e267759f92c2f9632c
karldmenzel <k.menzel15@gmail.com>
2025-12-07 18:56:03 -0600

    feature: make params random

--- client/main/client_main.go
+++ client/main/client_main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"math"
 	"math/rand/v2"
 	"sync"
 	"time"
@@ -20,16 +21,23 @@ func main() {
 	conn, server := connectToServer("localhost:50051")
 	defer conn.Close()
 
+	// Create the context object passed into each request.
 	requestContext, cancel := createRequestContext()
 	defer cancel()
 
+	// Make all 1000 requests concurrently.
 	make1000Requests(server, requestContext)
 
+	// Wait for all 1000 requests to finish.
 	waitGroup.Wait()
 
+	// Print the stats on how many times each function was called.
 	getCounters(server, requestContext)
 }
 
+// This function takes the URL of the server, and returns the raw connection object, and our server's object.
+// The connection object is returned strictly so that it can be closed by the main function.
+// The server object is what actually has the remote procedures exposed on it, and is what we will call.
 func connectToServer(serverAddress string) (*grpc.ClientConn, pb.MathServerClient) {
 
 	connection, err := grpc.NewClient(serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -42,12 +50,16 @@ func connectToServer(serverAddress string) (*grpc.ClientConn, pb.MathServerClien
 	return connection, server
 }
 
+// This function creates a context object which is passed in to all RPC requests.
+// For us, that context object just says that the request should time out after one second.
 func createRequestContext() (context.Context, context.CancelFunc) {
 	// All requests should time out after one second.
-	return context.WithTimeout(context.Background(), time.Second)
-
+	return context.WithTimeout(context.Background(), 1*time.Second)
 }
 
+// This function makes 1000 requests to the server for a random function.
+// Each call is made in a go routine, which all run concurrently.
+// These go routines are grouped in a 'wait group', which allows us to wait for them all to finish.
 func make1000Requests(server pb.MathServerClient, requestContext context.Context) {
 	for range 1000 {
 		methodId := rand.IntN(4)
@@ -67,31 +79,40 @@ func make1000Requests(server pb.MathServerClient, requestContext context.Context
 }
 
 func magicAdd(server pb.MathServerClient, requestContext context.Context) {
-	_, err := server.MagicAdd(requestContext, &pb.DoubleTerms{TermOne: 5.0, TermTwo: 2.0})
+	_, err := server.MagicAdd(requestContext, &pb.DoubleTerms{TermOne: randomDouble(), TermTwo: randomDouble()})
 	if err != nil {
 		panic("Error on MagicAdd.")
 	}
 }
 
 func magicSubtract(server pb.MathServerClient, requestContext context.Context) {
-	_, err := server.MagicSubtract(requestContext, &pb.DoubleTerms{TermOne: 10.0, TermTwo: 5.0})
+	_, err := server.MagicSubtract(requestContext, &pb.DoubleTerms{TermOne: randomDouble(), TermTwo: randomDouble()})
 	if err != nil {
 		panic("Error on MagicSubtract.")
 	}
 }
 
 func magicFindMin(server pb.MathServerClient, requestContext context.Context) {
-	_, err := server.MagicFindMin(requestContext, &pb.IntTerms{TermOne: 1.0, TermTwo: 2.0, TermThree: 5.0})
+	_, err := server.MagicFindMin(requestContext, &pb.IntTerms{TermOne: randomInt(), TermTwo: randomInt(), TermThree: randomInt()})
 	if err != nil {
 		panic("Error on MagicFindMin.")
 	}
 }
 
 func magicFindMax(server pb.MathServerClient, requestContext context.Context) {
-	_, err := server.MagicFindMax(requestContext, &pb.IntTerms{TermOne: 1.0, TermTwo: 2.0, TermThree: 5.0})
+	_, err := server.MagicFindMax(requestContext, &pb.IntTerms{TermOne: randomInt(), TermTwo: randomInt(), TermThree: randomInt()})
 	if err != nil {
 		panic("Error on MagicFindMin.")
 	}
+}
+
+func randomDouble() float64 {
+	// Here we divide by 2 so that if two large numbers are added they don't overflow
+	return rand.Float64() * (math.MaxFloat64 / 2)
+}
+
+func randomInt() int64 {
+	return rand.Int64() * math.MaxInt64
 }
 
 func getCounters(server pb.MathServerClient, requestContext context.Context) {
